eee7833ad12a3878fb1782413a3ae65e19e71dba
Krish Goel <krishgoel3@gmail.com>
2020-07-22 10:20:26 +0530

    FIxed nodemon error

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
+});
+
+router.get('/project', function(req, res, next) {
+  res.render('project', {});
+});
+
+router.get('/action', function(req, res, next) {
+  res.render('action', {});
+});
+
+router.get('/blog', function(req, res, next) {
+  res.render('blog', {});
 });
 
 module.exports = router;
